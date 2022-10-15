80ffcdcdfb049bc07b7f74db3c6a585a01bb116b
uwenorjeff <106762251+uwenorjeff@users.noreply.github.com>
2022-10-15 16:53:22 +0100

    new commit

--- factors_functions.py
+++ factors_functions.py
@@ -1,35 +1,21 @@
-#!/usr/bin/python3
+#/usr/bin/env python3
 import sys
-import ctypes
-from resource import getrusage as resource_usage, RUSAGE_SELF
-from time import time as timestamp
 
 
-def unix_time(function):
-    '''Return `real`, `sys` and `user` elapsed time, like UNIX's command `time`
-    You can calculate the amount of used CPU-time used by your
-    function/callable by summing `user` and `sys`. `real` is just like the wall
-    clock.
-    Note that `sys` and `user`'s resolutions are limited by the resolution of
-    the operating system's software clock (check `man 7 time` for more
-    details).
-    '''
-    start_time, start_resources = timestamp(), resource_usage(RUSAGE_SELF)
-    function()
-    end_resources, end_time = resource_usage(RUSAGE_SELF), timestamp()
-
-    return "\nreal: {}\nuser: {}\nsys: {}".format(
-        end_time - start_time,
-        end_resources.ru_utime - start_resources.ru_utime,
-        end_resources.ru_stime - start_resources.ru_stime)
+def primef(n):
+    if n <= 3:
+        return int(n)
+    if n % 2 == 0:
+        return 2
+    elif n % 3 == 0:
+        return 3
+    else:
+        for i in range(5, int((n)**0.5) + 1, 6):
+            if n % i == 0:
+                return int(i)
+            if n % (i + 2) == 0:
+                return primef(n/(i+2))
+    return int(n)
 
 
-def print_factors():
-    fun = ctypes.CDLL("./lib_factors_functions.so")
-    fun.trial_division.argtypes = [ctypes.c_long]
-    with open(sys.argv[1], 'r') as prime:
-        line = prime.readline()
-        while line != '':
-            n = int(line)
-            fun.trial_division(n)
-            line = prime.readline()
+print(primef(int(sys.argv[1])))
